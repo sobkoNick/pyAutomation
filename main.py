e6a0755a7d9b21d8b5d10a640ae5d3c51ee226c1
Mykola Sobko (EXT) <mykola.sobko.ext@360t.com>
2024-01-16 23:16:21 +0200

    added requirements.txt file. tried requests and pytest

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import requests
+import pytest
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -13,9 +14,31 @@ def one_more_print(name):
     print(f'Hello, {name} {name}')
 
 
+def make_request():
+    return requests.get("https://www.google.com.ua/")
+
+
+def loop():
+    count = 0
+    while count < 5:
+        count += 1
+        print(f"Iteration no. {count}")
+    else:
+        print("While loop over. Now in else block")
+    print("End of while loop")
+
+
+# to skip test
+# @pytest.mark.skip("outdated")
+def test_google():
+    response = make_request()
+    expected_code = 400
+    assert response.status_code == expected_code, \
+        f"Actual status code {response.status_code} differs from expected {expected_code}"
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    one_more_print('Function')
+    make_request()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
