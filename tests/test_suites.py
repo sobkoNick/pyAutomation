9972cd85abfc5a86f86fcafb2ac176bd80d7b4ed
Mykola Sobko <sobko.nick@gmail.com>
2024-01-27 23:43:05 +0200

    added @fixture(scope="module") and code to delete created suite after test execution.

--- tests/test_suites.py
+++ tests/test_suites.py
@@ -7,16 +7,24 @@ from models.suite_model import Suite
 from steps.api_client import ApiClient
 
 
-@fixture
+@fixture(scope="module")
 def default_suite(app):
     with open("tests/test_data/suite_to_post.json") as json_data:
         data = json.load(json_data)
+
+    # creates default suite for tests
     suite = ApiClient(token=app.token, endpoint=SUITES_ENDPOINT) \
         .post(url_params=[app.project_id], new_obj=data) \
         .validate_that().status_code_is_ok().get_response_body()
     suite = Suite.build(json.dumps(suite['data']))
     suite.attributes.description = ""
-    return suite
+
+    # passes suite into each test method in module
+    yield suite
+
+    # deletes the default fixture after run of all test methods in module
+    ApiClient(token=app.token, endpoint=SUITES_ENDPOINT) \
+        .delete(url_params=[app.project_id, suite.id]).validate_that().status_code_is_ok()
 
 
 def test_get_all_suites(app, default_suite):
@@ -26,4 +34,4 @@ def test_get_all_suites(app, default_suite):
         .status_code_is_ok() \
         .body_contains(default_suite)
 
-# TODO add test for all crud operations and response body validations in validator
+# TODO add test for all crud operations
