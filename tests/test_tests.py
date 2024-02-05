a0da63fc5a1a27e259229dbddddae333b4d0810a
Mykola Sobko <sobko.nick@gmail.com>
2024-02-05 13:16:42 +0200

    disabled unfinished test.

--- tests/test_tests.py
+++ tests/test_tests.py
@@ -1,6 +1,9 @@
+import pytest
+
 from steps.test_enpoint_steps import TestEndpointClient
 
 
+@pytest.mark.skip(reason="Unfinished test")
 def test_get_all_test(app):
     # TODO Redo after testing suites
     TestEndpointClient(app.token) \
